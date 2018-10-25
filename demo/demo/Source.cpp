68030b82ecd5c338536f9785af1d78c2486af416
Yunkai <Yunkai@LENOVO-PC>
2018-10-25 14:03:27 -0400

    aaa

--- demo/demo/Source.cpp
+++ demo/demo/Source.cpp
@@ -6,6 +6,15 @@ int main()
 {
 	std::cout << "lol" << std::endl;
 
+	std::cout << "kms" << std::endl;
+
+	while (true)
+	{
+		std::cout << "......" << std::endl;
+		int aa = 0;
+		aa = 5;
+	}
+
 	return 0;
 
 }
